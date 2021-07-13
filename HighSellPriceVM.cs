d2aa7711a8ee98152a9b326a19fa6a617363caf4
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2021-07-13 21:15:36 +0800

    Add files via upload

---
+++ HighSellPriceVM.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using SandBox.ViewModelCollection.Nameplate;
+
+namespace HighSellPrice
+{
+    [HarmonyPatch(typeof(SettlementNameplateVM), "RefreshDynamicProperties")]
+    public class HighSellPriceVM
+    {
+        // If the player has non-food trade goods of any number or food of more than 1 each with high selling prices, add an icon to the city's nameplate. If not, remove the icon.
+        public static void Postfix(SettlementNameplateVM __instance)
+        {
+            if (__instance.IsInRange && __instance.Settlement.IsTown)
+            {
+                ItemRoster itemRoster = MobileParty.MainParty.ItemRoster;
+                int num = 0;
+                for (int i = 0; i < itemRoster.Count; i++)
+                {
+                    ItemObject itemAtIndex = itemRoster.GetItemAtIndex(i);
+                    if ((!itemAtIndex.IsFood && itemAtIndex.IsTradeGood) || (itemAtIndex.IsFood && itemRoster.GetElementNumber(i) > 1))
+                    {
+                        ItemCategory itemCategory = itemAtIndex.ItemCategory;
+                        float num2 = 0f;
+                        float num3 = 0f;
+                        if (Town.AllTowns != null)
+                        {
+                            foreach (Town town in Town.AllTowns)
+                            {
+                                num2 += town.GetItemCategoryPriceIndex(itemCategory, false) + town.GetItemCategoryPriceIndex(itemCategory, false);
+                                num3 += 1f;
+                            }
+                        }
+                        num2 /= num3 * 2f;
+                        if (__instance.Settlement.Town.GetItemPrice(itemAtIndex, MobileParty.MainParty, true) / (float)itemAtIndex.Value > num2 * 1.3f)
+                        {
+                            num++;
+                        }
+                    }
+                }
+                SettlementEventsVM settlementEvents = __instance.SettlementEvents;
+                EventTypes = Enum.GetNames(typeof(SettlementNameplateEventItemVM.SettlementEventType)).Length;
+                SettlementNameplateEventItemVM settlementNameplateEventItem = settlementEvents.EventsList.FirstOrDefault((SettlementNameplateEventItemVM e) => e.EventType == (SettlementNameplateEventItemVM.SettlementEventType)EventTypes);
+                if (num > 0)
+                {
+                    if (!settlementEvents.EventsList.Contains(settlementNameplateEventItem))
+                    {
+                        settlementEvents.EventsList.Add(new SettlementNameplateEventItemVM((SettlementNameplateEventItemVM.SettlementEventType)EventTypes));
+                    }
+                }
+                else
+                {
+                    settlementEvents.EventsList.Remove(settlementNameplateEventItem);
+                }
+            }
+        }
+        public static int EventTypes { get; set; }
+    }
+}
---
+++ HighSellPriceWidget.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using TaleWorlds.GauntletUI;
+using TaleWorlds.MountAndBlade.GauntletUI.Widgets.Nameplate;
+
+namespace HighSellPrice
+{
+    [HarmonyPatch(typeof(SettlementNameplateEventVisualWidget), "UpdateVisual")]
+    public class HighSellPriceWidget
+    {
+        // Set the icon widget to a red coin.
+        private static void Prefix(int type, BrushWidget __instance)
+        {
+            if (type == HighSellPriceVM.EventTypes)
+            {
+                __instance.SetState("HighSellingPrice");
+                return;
+            }
+        }
+    }
+}
